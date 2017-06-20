8b6950faf13a6031226efe436999c6bf4241ab7b
lujanfernaud <lujanfernaud@gmail.com>
2017-06-20 22:59:38 +0100

    Extract PathChecker class from Board class

---
+++ lib/chess/path_checker.rb
@@ -0,0 +1,108 @@
+# Checks if there are no pieces from the origin to the destination.
+class PathChecker < Board
+  attr_reader :grid, :piece
+  attr_reader :from_row, :to_row, :from_column, :to_column
+
+  DIRECTIONS = { row:    { down:  1, up:   -1 },
+                 column: { left: -1, right: 1 } }.freeze
+
+  def initialize(grid, piece, from, to)
+    @grid        = grid
+    @piece       = piece
+    @from_row    = from[0]
+    @from_column = from[1]
+    @to_row      = to[0]
+    @to_column   = to[1]
+    @path        = []
+  end
+
+  def empty_path?
+    return true if piece.is_a?(Knight)
+
+    add_directions_to_path
+    add_diagonals_to_path
+    check_path
+  end
+
+  private
+
+  attr_accessor :path
+
+  def add_directions_to_path
+    return add_columns_in_direction(:left)  if same_row && left
+    return add_columns_in_direction(:right) if same_row && right
+    return add_rows_in_direction(:up)       if same_column && up
+    return add_rows_in_direction(:down)     if same_column && down
+  end
+
+  def add_diagonals_to_path
+    return add_squares_in_diagonal(:down, :right) if down && right
+    return add_squares_in_diagonal(:down, :left)  if down && left
+    return add_squares_in_diagonal(:up, :right)   if up && right
+    return add_squares_in_diagonal(:up, :left)    if up && left
+  end
+
+  def check_path
+    empty = proc { |position| grid[position[0]][position[1]] == "-" }
+    path.all?(&empty)
+  end
+
+  def same_row
+    from_row == to_row
+  end
+
+  def same_column
+    from_column == to_column
+  end
+
+  def left
+    from_column > to_column
+  end
+
+  def right
+    from_column < to_column
+  end
+
+  def up
+    from_row > to_row
+  end
+
+  def down
+    from_row < to_row
+  end
+
+  def add_columns_in_direction(direction)
+    column_step = DIRECTIONS[:column][direction]
+
+    from_column.step(to_column, column_step) do |column|
+      next  if column == from_column
+      break if column == to_column
+      path << [to_row, column]
+    end
+  end
+
+  def add_rows_in_direction(direction)
+    row_step = DIRECTIONS[:row][direction]
+
+    from_row.step(to_row, row_step) do |row|
+      next  if row == from_row
+      break if row == to_row
+      path << [row, to_column]
+    end
+  end
+
+  def add_squares_in_diagonal(row_direction, column_direction)
+    row_step    = DIRECTIONS[:row][row_direction]
+    column_step = DIRECTIONS[:column][column_direction]
+
+    from_row.step(to_row, row_step) do |row|
+      next  if row == from_row
+      break if row == to_row
+      from_column.step(to_column, column_step) do |column|
+        next  if column == from_column
+        break if column == to_column
+        path << [row, column]
+      end
+    end
+  end
+end

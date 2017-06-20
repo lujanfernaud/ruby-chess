25b53e2ac2730ef3a7afc420be71283675b211e0
lujanfernaud <lujanfernaud@gmail.com>
2017-06-20 18:09:59 +0100

    Check if the path is empty

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -16,6 +16,7 @@ class Board
     piece = get_piece(from)
 
     return move_not_possible unless move_possible?(piece, from, to)
+    return pieces_in_between unless empty_path?(piece, from, to)
 
     remove_piece(from)
     place_piece(piece, to)
@@ -63,6 +64,7 @@ class Board
   # to their positions in the grid.
   # Letter represents column and number represents row.
   # Example: "a2" would be translated as [6, 0]
+
   def translate_coords(coords)
     letter = coordinates[coords[0]]
     number = coordinates[coords[1]]
@@ -87,5 +89,77 @@ class Board
 
   def move_possible?(piece, from, to)
     piece.allowed_move?(from, to)
+  end
+
+  def pieces_in_between
+    "There are pieces in between."
+  end
+
+  def empty_path?(piece, from, to)
+    return true if piece.is_a?(Knight)
+
+    from_row    = from[0]
+    from_column = from[1]
+    to_row      = to[0]
+    to_column   = to[1]
+    way         = []
+
+    if from_row == to_row && from_column > to_column
+      from_column.step(to_column, -1) do |column|
+        next if column == from_column
+        way << [to_row, column]
+      end
+    elsif from_row == to_row && from_column < to_column
+      from_column.step(to_column, 1) do |column|
+        next if column == from_column
+        way << [to_row, column]
+      end
+    elsif from_column == to_column && from_row > to_row
+      from_row.step(to_row, -1) do |row|
+        next if row == from_row
+        way << [row, to_column]
+      end
+    elsif from_column == to_column && from_row < to_row
+      from_row.step(to_row, 1) do |row|
+        next if row == from_row
+        way << [row, to_column]
+      end
+    elsif from_row < to_row && from_column < to_column
+      from_row.step(to_row, 1) do |row|
+        next if row == from_row
+        from_column.step(to_column, 1) do |column|
+          next if column == from_column
+          way << [row, column]
+        end
+      end
+    elsif from_row < to_row && from_column > to_column
+      from_row.step(to_row, 1) do |row|
+        next if row == from_row
+        from_column.step(to_column, -1) do |column|
+          next if column == from_column
+          way << [row, column]
+        end
+      end
+    elsif from_row > to_row && from_column < to_column
+      from_row.step(to_row, -1) do |row|
+        next if row == from_row
+        from_column.step(to_column, 1) do |column|
+          next if column == from_column
+          way << [row, column]
+        end
+      end
+    elsif from_row > to_row && from_column > to_column
+      from_row.step(to_row, -1) do |row|
+        next if row == from_row
+        from_column.step(to_column, -1) do |column|
+          next if column == from_column
+          way << [row, column]
+        end
+      end
+    end
+
+    empty = proc { |square| grid[square[0]][square[1]] == "-" }
+
+    way[0..-2].all?(&empty)
   end
 end

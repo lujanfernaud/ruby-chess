135c4172397f49076318913d399d6df3b783b64a
lujanfernaud <lujanfernaud@gmail.com>
2017-07-06 11:53:44 +0100

    Save current player and next player in Game class

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -1,13 +1,16 @@
 # Controls game flow.
 class Game
   attr_reader :board, :screen, :player1, :player2, :players
+  attr_reader :current_player, :next_player
 
   def initialize(player1, player2)
-    @board   = Board.new(self)
-    @screen  = Screen.new(board)
-    @player1 = player1
-    @player2 = player2
-    @players = [player1, player2]
+    @board          = Board.new(self)
+    @screen         = Screen.new(board)
+    @player1        = player1
+    @player2        = player2
+    @players        = [player1, player2]
+    @current_player = nil
+    @next_player    = nil
   end
 
   def start
@@ -32,11 +35,21 @@ class Game
   def players_turns
     loop do
       players.each do |player|
-        screen.print_board
-        movement = input(player)
-        board.move_piece(player, movement)
+        save_players_based_on(player)
+        player_turn(player)
       end
     end
+  end
+
+  def save_players_based_on(player)
+    @current_player = player
+    @next_player    = players.reject { |plyr| plyr == player }
+  end
+
+  def player_turn(player)
+    screen.print_board
+    movement = input(player)
+    board.move_piece(player, movement)
   end
 
   def input(player)
